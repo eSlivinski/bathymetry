3a6343c559b1aae189d167b384b562a0c9e3ce1f
eSlivinski <eslivinski@gmail.com>
2017-04-16 03:52:47 -0400

    resolve dependency installs

--- application/public_src/scripts/dependencies.js
+++ application/public_src/scripts/dependencies.js
@@ -2,5 +2,3 @@ d3 = require('d3'),
 L = require('leaflet');
 _ = require('lodash');
 turfSimplify = require('turf-simplify');
-turfLineToPolygon = require('turf-line-to-polygon');
-turfBuffer = require('turf-buffer');
