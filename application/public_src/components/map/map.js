6b1912e23ecac21f98a9b117a5f2620b22419259
eSlivinski <eslivinski@gmail.com>
2017-04-16 14:11:45 -0400

    popups

--- application/public_src/components/map/map.js
+++ application/public_src/components/map/map.js
@@ -1,8 +1,9 @@
-this.on('mount', function () {
+this.on('mount', () => {
   var map = L.map('map', {
     center: [60.3764, -152.9311],
     zoom: 13
-  }).on('zoomend', function() {
+  });
+  map.on('zoomend', () => {
     d3.selectAll('path')
       .attr('d', path);
   });
@@ -16,29 +17,29 @@ this.on('mount', function () {
 
   L.svg({ clickable: true }).addTo(map);
 
-  var svg = d3.select('#map').select('svg')
-    .attr('pointer-events', 'auto');
-
+  var svg = d3.select('#map').select('svg');
   var g = svg.select('g');
 
   var transform = d3.geoTransform({
-        point: function (x, y) {
-          var point = map.latLngToLayerPoint(new L.LatLng(y, x));
-          this.stream.point(point.x, point.y);
-        }
-      }),
-      path = d3.geoPath().projection(transform);
+    point: function (x, y) {
+      var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+      this.stream.point(point.x, point.y);
+    }
+  });
+  var path = d3.geoPath().projection(transform);
 
-
-  d3.json('data/focal-10-contour-10.geojson', function (err, data) {
+  d3.json('data/focal-10-contour-10.geojson', (err, data) => {
     if (err) { return console.error(err); }
 
-    var deepest = _.min(_.map(data.features, function (d) { return d.properties.Contour; }));
+    var deepest = _.min(_.map(data.features, d => { return d.properties.Contour; }));
 
     colorScale.domain([0, deepest]);
 
     var contours = _.chain(data.features)
-      .map(function(d) { return turfSimplify(d, 0.0001, false); })
+      .map(d => {
+        d.properties.color = colorScale(d.properties.Contour);
+        return turfSimplify(d, 0.0001, false);
+      })
       .orderBy(['properties', 'Contour'])
       .reverse()
       .value();
@@ -46,16 +47,25 @@ this.on('mount', function () {
     g.selectAll('path')
       .data(contours)
       .enter().append('path')
-      .attr('d', path)
-      .attr('stroke-width', 0)
-      .attr('stroke', function (feature) { return colorScale(feature.properties.Contour); })
-      .attr('fill', function (feature) { return colorScale(feature.properties.Contour); })
-      .attr('fill-opacity', 0)
-      .attr('stroke-linejoin', 'round')
+        .attr('d', path)
+        .attr('class', 'contour')
+        .attr('stroke', d => { return d.properties.color; })
+        .attr('fill', d => { return d.properties.color; })
+        .attr('stroke-width', 0)
+        .attr('fill-opacity', 0)
+        .on('click', contourClicked)
       .transition().delay(500)
-      .attr('fill-opacity', 0.5)
+        .attr('fill-opacity', 0.5)
       .transition().delay(500)
-      .attr('stroke-width', 3);
-
+        .attr('stroke-width', 3);
   });
+
+  function contourClicked (d) {
+    var latlng = map.mouseEventToLatLng(d3.event);
+    var popup = L.popup()
+      .setLatLng(latlng)
+      .setContent('Depth: ~' + Math.abs(d.properties.Contour) + 'ft.');
+
+    setTimeout(() => { map.openPopup(popup); }, 200);
+  }
 });
